ca2d7fd4ef6c941e7ecca39e0f023b5c445b7f6b
Golam Sarwer <golamsarwer97@gmail.com>
2026-03-12 15:42:10 +0600

    added account details view

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.urls import path
 
 from myapp.views import (home_screen_view)
-from account.views import (registration_view, logout_view, login_view)
+from account.views import (registration_view, logout_view, login_view, account_view)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', registration_view, name='register'),
     path('logout/', logout_view, name='logout'),
     path('login/', login_view, name='login'),
+    path('account/', account_view, name='account'),
     # path('polls/', include('myapp.urls')), include,
     path("",home_screen_view,name="home"),
     # path("index",index,name="index")
